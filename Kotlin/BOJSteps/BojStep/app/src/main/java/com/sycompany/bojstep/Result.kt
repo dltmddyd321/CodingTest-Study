5ef03e961ae533c9c61633244a3f93e415a76ff7
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2022-09-18 23:03:26 -0700

    셀프 넘버

--- Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/Result.kt
+++ Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/Result.kt
@@ -172,3 +172,37 @@ fun average() = with(Scanner(System.`in`)){
         System.out.printf("%.3f%s\n", cnt.toDouble() / size * 100, "%")
     }
 }
+
+//4673
+fun selfNumber() {
+    val max = 10000
+    val arr = IntArray(max)
+    var ori = 1
+
+    fun d(n: Int) {
+        var num = n
+        var sum = n
+
+        while (num > 0) {
+            sum += num%10
+            num /= 10
+        }
+
+        if (sum >= max) {
+            return
+        } else {
+            arr[sum]++
+            d(sum)
+        }
+    }
+
+    while (ori< max) {
+        d(ori++)
+    }
+
+    for (i in arr.indices) {
+        if (i != 0 && arr[i] == 0) {
+            println(i)
+        }
+    }
+}
