9f92e8d4dde0dbbad11c51913ec36334208b7230
dltmddyd321 <dltmddyd321@naver.com>
2022-09-27 22:03:30 +0900

    2022-09-27
    - 최대공약수 최소공배수
    - 시저암호
    - 내적

--- Kotlin/ProgremmersLevelOne/app/src/main/java/com/example/progremmerslevelone/LevelOne.kt
+++ Kotlin/ProgremmersLevelOne/app/src/main/java/com/example/progremmerslevelone/LevelOne.kt
@@ -1,5 +1,7 @@
 package com.example.progremmerslevelone
 
+import kotlin.math.sqrt
+
 //정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요.
 // 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요.
 // 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
@@ -73,4 +75,51 @@ fun divisionArr(arr: IntArray, divisor: Int): IntArray {
     temp.sort()
     answer = temp.toIntArray()
     return answer
+}
+
+//정수 제곱근 판별
+fun doubleCheck(n: Long): Long {
+    val a = sqrt(n.toDouble()).toLong()
+    return if (a*a == n) (a+1)*(a+1) else -1
+}
+
+//내적
+fun solution(a: IntArray, b: IntArray): Int {
+    var answer = 0
+    for (i in a.indices) {
+        answer += a[i]*b[i]
+    }
+    return answer
+}
+
+//최대공약수와 최소공배수 -> 유클리드 호제법
+fun solution(n: Int, m: Int): IntArray = intArrayOf(gcd(n, m), lcm(n, m))
+
+//최대공약수
+fun gcd(n: Int, m: Int): Int {
+    return if (n < m) {
+        if (n == 0) m else gcd(n, m % n)
+    } else {
+        if (m == 0) n else gcd(m, n % m)
+    }
+}
+
+//최소공배수
+fun lcm(n: Int, m: Int) = n * m / gcd(n, m)
+
+//시저 암호
+//공백은 아무리 밀어도 공백이다.
+fun solution(s: String, n: Int): String {
+    return s.toCharArray().map {
+        when(it) {
+            in 'A'..'Z' -> {
+                if (it + n > 'Z') it + n - 26 else it + n
+            }
+            in 'a'..'z' -> {
+                if (it + n > 'z') it + n - 26 else it + n
+            }
+            else -> { it }
+        }
+        //공백없이 문자열 연결
+    }.joinToString("")
 }
