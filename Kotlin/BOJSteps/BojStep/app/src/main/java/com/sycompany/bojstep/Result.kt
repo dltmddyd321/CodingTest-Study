a2c69396c642b77b98f6f28e4757cdc347ee8260
dltmddyd321 <dltmddyd321@naver.com>
2022-11-01 20:21:47 +0900

    Zero - 스택 문제

--- Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/Result.kt
+++ Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/Result.kt
@@ -508,3 +508,17 @@ fun statistics() {
         return maxValue - minValue
     }
 }
+
+fun zeroStack() = with(System.`in`.bufferedReader()) {
+    val k = readLine().toInt()
+    val stack = arrayListOf<Int>()
+
+    for (i in 0 until k) {
+        val temp = readLine().toInt()
+        if (temp == 0) stack.removeAt(stack.size - 1)
+        else stack.add(temp)
+    }
+    println(stack.sum())
+}
+
+
