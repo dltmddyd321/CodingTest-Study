a18d4adccb739827b5a9681e9c7f3abd37e439e7
이승용 <dltmddyd321@naver.com>
2022-12-14 16:36:16 +0900

    파일 확장자명 확인하기

--- Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/MainActivity.kt
+++ Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/MainActivity.kt
@@ -2,10 +2,19 @@ package com.sycompany.bojstep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.webkit.MimeTypeMap
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val longStr = getString(R.string.long_str)
+        val fileExtension = MimeTypeMap.getFileExtensionFromUrl(longStr)
+
+        val result = longStr.substring(longStr.lastIndexOf("."))
+
+        Log.d("확장자명", "file ext : $result")
     }
 }
