5e6d89c29be7dabbdc32aeae4cf9ab190d440acd
이승용 <dltmddyd321@naver.com>
2022-12-28 11:43:49 +0900

    registerForActivityResult

--- Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/MainActivity.kt
+++ Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.sycompany.bojstep
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.webkit.MimeTypeMap
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var textName : TextView
+    private lateinit var textAge : TextView
+    private lateinit var nextBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val longStr = getString(R.string.long_str)
-        val fileExtension = MimeTypeMap.getFileExtensionFromUrl(longStr)
+        textName = findViewById(R.id.textName)
+        textAge = findViewById(R.id.textAge)
+        nextBtn = findViewById(R.id.nextBtn)
 
-        val result = longStr.substring(longStr.lastIndexOf("."))
+        initResultText()
 
-        Log.d("확장자명", "file ext : $result")
+        nextBtn.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            resultLauncher.launch(intent) // startActivityForResult 랑 동일한 기능 이다.
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun initResultText() {
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val name = it.data?.getStringExtra("name") ?: ""
+                val age = it.data?.getStringExtra("age") ?: ""
+
+                textName.text = "이름 : $name"
+                textAge.text = "나이 : $age 살"
+            }
+        }
     }
 }
