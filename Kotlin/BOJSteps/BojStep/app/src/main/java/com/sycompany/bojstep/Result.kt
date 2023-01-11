39a922754dfe86e26ed3175a493c6413788c55f2
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-01-10 22:19:25 -0800

    프로그래머스 - 구슬을 나누는 경우의 수
    - 필요하다면 그냥 암기해야 할듯..

--- Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/Result.kt
+++ Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/Result.kt
@@ -1125,3 +1125,12 @@ fun checkLogin(id_pw: Array<String>, db: Array<Array<String>>): String {
         "wrong pw"
     } else "fail"
 }
+
+fun combination(n: Int, r: Int): Int {
+    return if (r == 0 || n == r) 1
+    else combination(n-1, r-1) + combination(n-1, r)
+}
+
+fun solutionCom(balls: Int, share: Int): Int {
+    return combination(balls, share)
+}
