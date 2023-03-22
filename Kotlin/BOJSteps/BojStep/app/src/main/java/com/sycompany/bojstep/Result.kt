7741f9dbce45c1a5e886ccfc786df774fc9db9a8
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-03-21 21:06:01 -0700

    모스부호1

--- Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/Result.kt
+++ Kotlin/BOJSteps/BojStep/app/src/main/java/com/sycompany/bojstep/Result.kt
@@ -1490,3 +1490,35 @@ fun chickenCoupon(chicken: Int): Int {
     }
     return res
 }
+
+fun mosCheck(letter: String): String {
+    val dictionary = mapOf(
+        ".-" to "a",
+        "-..." to "b",
+        "-.-." to "c",
+        "-.." to "d",
+        "." to "e",
+        "..-." to "f",
+        "--." to "g",
+        "...." to "h",
+        ".." to "i",
+        ".---" to "j",
+        "-.-" to "k",
+        ".-.." to "l",
+        "--" to "m",
+        "-." to "n",
+        "---" to "o",
+        ".--." to "p",
+        "--.-" to "q",
+        ".-." to "r",
+        "..." to "s",
+        "-" to "t",
+        "..-" to "u",
+        "...-" to "v",
+        ".--" to "w",
+        "-..-" to "x",
+        "-.--" to "y",
+        "--.." to "z"
+    )
+    return letter.split(" ".toRegex()).map { dictionary[it] }.joinToString("")
+}
