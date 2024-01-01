7a412b468fe76930ae4cf093df81d575833a9d39
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-01-01 20:46:32 +0900

    [level 1] Title: 숫자 짝꿍, Time: 19.29 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
@@ -2,7 +2,7 @@ class Solution {
     fun solution(X: String, Y: String): String = X.filter { Y.contains(it) }
         .groupBy { it }
         .map {if (it.value.size < Y.groupBy { it }[it.key]!!.size) it.value else Y.groupBy { it }[it.key]!!}
-        .flatten()
+        .flatten() //중첩 컬렉션을 모두 열어 하나의 배열로 정리한다.
         .sortedByDescending { it }
         .joinToString("")
         .let { it.ifEmpty { "-1" } }
