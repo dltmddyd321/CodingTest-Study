76b5eb49d1a00f3a0af74ba1907da84d5360967e
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-01-22 21:27:23 +0900

    [Bronze V] Title: 사분면 고르기, Time: 124 ms, Memory: 13512 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    val a = nextInt()
+    val b = nextInt()
+
+    if (a > 0 && b > 0) println(1)
+    if (a < 0 && b > 0) println(2)
+    if (a < 0 && b < 0) println(3)
+    if (a > 0 && b < 0) println(4)
+}
