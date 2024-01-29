cb35dbc9107e4575d681a591ee3ae41fd3d236c0
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-01-29 21:00:50 +0900

    [Bronze II] Title: 대표값2, Time: 176 ms, Memory: 20924 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.kt
@@ -0,0 +1,15 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun main() = with(Scanner(System.`in`)) {
+    val arr = IntArray(5)
+
+    for (i in 0 until 5) {
+        arr[i] = readln().toInt()
+    }
+
+    val sortedArray = arr.sorted()
+
+    println(sortedArray.sum() / 5)
+    println(sortedArray[2])
+}
