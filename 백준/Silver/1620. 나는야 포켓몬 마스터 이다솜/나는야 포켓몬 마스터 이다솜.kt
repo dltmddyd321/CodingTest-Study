43c44996afad67edb5da8efbe8a2efac62b741c8
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-02-16 21:30:23 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 640 ms, Memory: 75756 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.kt
@@ -0,0 +1,34 @@
+
+import java.util.*
+import kotlin.collections.ArrayList
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val (cntData, cntQuestion) = br.readLine().split(" ").map { it.toInt() }
+    val numberKeys = mutableMapOf<Int, String>()
+    val nameKeys = mutableMapOf<String, Int>()
+
+    repeat(cntData) {
+        val name = br.readLine()
+        numberKeys[it + 1] = name
+        nameKeys[name] = it + 1
+    }
+
+    repeat(cntQuestion) {
+        val input = br.readLine()
+
+        if (input[0].isDigit()) {
+            bw.write("${numberKeys[input.toInt()]}\n")
+        } else {
+            bw.write("${nameKeys[input]}\n")
+        }
+    }
+    bw.flush()
+    bw.close()
+}
