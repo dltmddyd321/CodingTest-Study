b064bb3741f455962219dac4bbaf8c5a0d7f0667
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-03-06 21:35:27 +0900

    [Silver II] Title: 에디터, Time: 440 ms, Memory: 83308 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.kt
@@ -0,0 +1,46 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Scanner
+import java.util.Stack
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val leftStack = readLine().map { it.toString() }.toMutableList()
+    val rightStack = mutableListOf<String>() // 리스트로 스택 구현
+    val n = readLine().toInt()
+
+    repeat(n) {
+        when (val cmd = readLine()) {
+            "L" -> {
+                if (leftStack.isNotEmpty()) {
+                    rightStack.add(leftStack.removeAt(leftStack.lastIndex))
+                }
+            }
+
+            "D" -> {
+                if (rightStack.isNotEmpty()) {
+                    leftStack.add(rightStack.removeAt(rightStack.lastIndex))
+                }
+            }
+
+            "B" -> {
+                if (leftStack.isNotEmpty()) {
+                    leftStack.removeAt(leftStack.lastIndex)
+                }
+            }
+
+            else -> {
+                leftStack.add(cmd[2].toString())
+            }
+        }
+    }
+
+    val answer = StringBuilder()
+    leftStack.forEach { answer.append(it) }
+    for (i in rightStack.lastIndex downTo 0) { // 오른쪽 스택은 top부터 문자를 읽어야 한다
+        answer.append(rightStack[i])
+    }
+    print(answer)
+}
