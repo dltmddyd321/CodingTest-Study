7b27511f20e7a5a3c9d84362a0a7cf0d4de0f25f
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-03-14 20:51:56 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 104 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Scanner
+
+fun main(){
+    var a=readLine()
+    while(a!=null){
+        println(a)
+        a=readLine()
+    }
+}
