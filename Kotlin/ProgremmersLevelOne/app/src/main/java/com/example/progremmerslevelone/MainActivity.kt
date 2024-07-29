07b4e61a84c6f2fa480b5092b36f129b95f560ef
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-07-29 13:57:14 +0900

    프로그래머스 - 기능 개발 풀이

--- Kotlin/ProgremmersLevelOne/app/src/main/java/com/example/progremmerslevelone/MainActivity.kt
+++ Kotlin/ProgremmersLevelOne/app/src/main/java/com/example/progremmerslevelone/MainActivity.kt
@@ -9,3 +9,56 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+fun main(progresses: IntArray, speeds: IntArray): IntArray { //오답
+    val stack = Stack<Pair<Int, Int>>()
+
+    progresses.forEachIndexed { index, value ->
+        val needProgress = (100 - value)
+        val needDays = if (needProgress % speeds[index] != 0) {
+            needProgress / speeds[index] + 1
+        } else {
+            needProgress / speeds[index]
+        }
+        if (stack.isNotEmpty()) {
+            val first = stack.peek()
+            if (needDays > first.first) {
+                stack.push(Pair(needDays, 1))
+            } else {
+                stack.pop()
+                stack.push(Pair(needDays, first.second + 1))
+            }
+        } else {
+            stack.push(Pair(needDays, 1))
+        }
+    }
+    return stack.map { it.second }.toIntArray()
+}
+
+fun solution(progresses: IntArray, speeds: IntArray): IntArray { //정답
+    val daysToComplete = progresses.mapIndexed { index, progress ->
+        val remainingProgress = 100 - progress
+        if (remainingProgress % speeds[index] == 0) {
+            remainingProgress / speeds[index]
+        } else {
+            remainingProgress / speeds[index] + 1
+        }
+    }
+
+    val result = mutableListOf<Int>()
+    var maxDays = daysToComplete[0]
+    var count = 1
+
+    for (i in 1 until daysToComplete.size) {
+        if (daysToComplete[i] <= maxDays) {
+            count++
+        } else {
+            result.add(count)
+            maxDays = daysToComplete[i]
+            count = 1
+        }
+    }
+    result.add(count)
+
+    return result.toIntArray()
+}
