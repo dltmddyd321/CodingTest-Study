f6b19eb3d04e12f1c155e5dc53be3f0aa1a6b4b8
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-08-16 21:08:52 +0900

    [level 1] Title: 달리기 경주, Time: 436.48 ms, Memory: 349 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
 
 ### 성능 요약
 
-메모리: 61.5 MB, 시간: 0.04 ms
+메모리: 349 MB, 시간: 436.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 22:7:47
+2024년 08월 16일 21:08:49
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.kt
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.kt
@@ -1,19 +1,5 @@
 class Solution {
     fun solution(players: Array<String>, callings: Array<String>): Array<String> {
-//         val callingPointsMap = callings.groupBy { it }.mapValues { it.value.size }
-//         val playersPos = players.indices.associateBy { players[it] }.toMutableMap()
-
-//         callingPointsMap.forEach {
-//             //이동 대상의 기존 인덱스를 먼저 구한다.
-//             val initPos = playersPos[it.key] ?: 0
-// https://small-stepping.tistory.com/m/232#            val changedPos = initPos - it.value
-
-//             val temp = players[changedPos]
-//             players[changedPos] = it.key
-//             players[initPos] = temp
-//         }
-//         return players
-        
         val rankMap : HashMap<String, Int> = hashMapOf()
 
         //원래 플레이어들의 위치 값을 정리한다.
