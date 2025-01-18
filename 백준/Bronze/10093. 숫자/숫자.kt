c5e5b8105851640b35605463dc5542fcbc4d74c2
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-01-18 14:31:38 +0900

    [Bronze II] Title: 숫자, Time: 724 ms, Memory: 35416 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val num = readln().split(" ").map { it.toLong() }
+    val n = num.min()
+    val m = num.max()
+
+    if (m - n <= 1) {
+        println(0)
+    } else {
+        println(m - n - 1)
+        for (i in n + 1 until m) print("$i ")
+    }
+}
